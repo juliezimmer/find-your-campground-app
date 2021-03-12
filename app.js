a1e39a8f1f6afb3d1ea9a04c72680e5e4767f296
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2021-03-11 20:13:07 -0600

    added campground index view

--- app.js
+++ app.js
@@ -23,11 +23,10 @@ app.get('/', (req, res) => {
    res.render('home');
 })
 
-// make a new campground //
-app.get('/makecampground', async (req, res) => {
-   const camp = new Campground({title:'My Backyard', description: 'cheap camping'});
-   await camp.save();
-   res.send(camp);
+app.get('/campgrounds', async (req, res) => {
+   const campgrounds = await Campground.find({});
+   res.render('campgrounds/index', { campgrounds });
+
 })
 
 
