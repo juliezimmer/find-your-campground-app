d8434a651a8dd6a599d726a98d5b17b5c0a88874
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2021-03-12 19:26:34 -0600

    added a form to add a camp ground, and links to move between pages

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground')
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, 
-useCreateIndex: true,
-useUnifiedTopology: true
+   mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, 
+   useCreateIndex: true,
+   useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
@@ -19,6 +19,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
    res.render('home');
 })
@@ -26,8 +28,25 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
    const campgrounds = await Campground.find({});
    res.render('campgrounds/index', { campgrounds });
-
 })
+
+app.get('/campgrounds/new', (req, res) => {
+   res.render('campgrounds/new');
+})
+
+// endpoint where new campground form is submitted //
+app.post('/campgrounds', async(req, res) => {
+   const campground = new Campground(req.body.campground);
+   await campground.save();
+   res.redirect(`/campgrounds/${campground._id}`);
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+   const campground = await Campground.findById(req.params.id);
+   res.render('campgrounds/show', { campground });
+})
+
+
 
 
 app.listen(3000, () => {
