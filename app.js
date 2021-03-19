57357728c6c9ba92f1293e66eea0bc14c8718d85
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2021-03-19 16:05:57 -0500

    created error handling class and iplemented in app.js; created error ejs template

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utilities/catchAsync');
+const ExpressError = require('./utilities/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground')
 
@@ -43,6 +44,7 @@ app.get('/campgrounds/new', (req, res) => {
 
 // endpoint where new campground form is submitted //
 app.post('/campgrounds', catchAsync(async(req, res, next) => {
+   if(! req.body.campgrounds) throw new ExpressError('Invalid Campground Data', 400);
    const campground = new Campground(req.body.campground);
    await campground.save();
    // redirects to detail page //
@@ -76,9 +78,20 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
    res.redirect('/campgrounds');
 }))
 
+// Uses Error Class //
+// covers all paths //
+app.all('*', (req, res, next) => {
+   next(new ExpressError("page not found", 404)) // can use in app.use() //
+})
+
 // error handling function //
+// ExpressError is passed down to this function to process //
 app.use((err, req, res, next) => {
-   res.send("Something went wrong!");
+   // destructure from error and provide default value //
+   const { statusCode = 500 } = err;
+   if(!err.message) err.message = "Something went wrong"
+   // statusCode and message are now available for use //
+   res.status(statusCode).render('error', { err });
 })
 
 
