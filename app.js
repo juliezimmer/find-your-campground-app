d0217225e300d5fb34b46d618f482821d384314d
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2021-04-13 19:56:46 -0500

    added .env file with variables

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ if(process.env.NODE_ENV !== 'production'){
    require('dotenv').config();
 }
 
-
+console.log(process.env.SECRET);
+console.log(process.env.API);
 
 const express = require('express');
 const path = require('path');
