ce03fffe5869648362ed2b081fd821c37b995aa0
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2021-04-24 15:26:31 -0500

    configured helmet CSP

--- app.js
+++ app.js
@@ -64,7 +64,51 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
-app.use(helmet({contentSecurityPolicy: false}));
+app.use(helmet());
+
+const scriptSrcUrls = [
+   "https://stackpath.bootstrapcdn.com/",
+   "https://api.tiles.mapbox.com/",
+   "https://api.mapbox.com/",
+   "https://kit.fontawesome.com/",
+   "https://cdnjs.cloudflare.com/",
+   "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+   "https://kit-free.fontawesome.com/",
+   "https://api.mapbox.com/",
+   "https://api.tiles.mapbox.com/",
+   "https://fonts.googleapis.com/",
+   "https://use.fontawesome.com/",
+   "https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = [
+   "https://api.mapbox.com/",
+   "https://a.tiles.mapbox.com/",
+   "https://b.tiles.mapbox.com/",
+   "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use( // configuring helmet CSP
+   helmet.contentSecurityPolicy({
+       directives: {
+           defaultSrc: [],
+           connectSrc: ["'self'", ...connectSrcUrls],
+           scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+           styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+           workerSrc: ["'self'", "blob:"],
+           objectSrc: [],
+           imgSrc: [
+               "'self'",
+               "blob:",
+               "data:",
+               "https://res.cloudinary.com/dyybhhmce/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+               "https://images.unsplash.com/",
+           ],
+           fontSrc: ["'self'", ...fontSrcUrls],
+       },
+   })
+);
 
 // initialize and use passport //
 app.use(passport.initialize());
